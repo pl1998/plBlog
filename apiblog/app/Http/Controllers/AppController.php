d28c55306b46414081d3ea9e19c923228d720fb3
pl1998 <2139714249@qq.com>
2021-10-11 18:10:12 +0800

    调整

--- apiblog/app/Http/Controllers/AppController.php
+++ apiblog/app/Http/Controllers/AppController.php
@@ -53,6 +53,26 @@ class AppController extends Controller
 
     public function wxCallback(Request $request)
     {
+
+
+        ob_clean ();
+        //1. 将timestamp , nonce , token 按照字典排序
+        $timestamp = $_GET['timestamp'];
+        $nonce = $_GET['nonce'];
+        $token = "";
+        //token
+        $signature = $_GET['signature'];
+        $array = array($timestamp,$nonce,$token);
+        sort($array);
+        //2.将排序后的三个参数拼接后用sha1加密
+        $tmpstr = implode('',$array);
+        $tmpstr = sha1($tmpstr);
+        //3. 将加密后的字符串与 signature 进行对比, 判断该请求是否来自微信
+        if($tmpstr == $signature) {
+            echo $_GET['echostr'];
+            exit;
+        }
+
         #token wZwkDx7AKGzluE2lbN6
         #EncodingAESKey wZwkDx7AKGzluE2lbN6UEEPzPLx7YUpz3T7tDhmPcvc
 
